a5314bdcafc2c1f3fc5d54c94dd2bc5ac5363d62
Vitaliy S <lokizor@mail.ru>
2025-07-19 15:54:24 +0300

    Added personal advices

--- bot/handlers.py
+++ bot/handlers.py
@@ -12,7 +12,7 @@ from aiogram.filters.state import StateFilter
 from aiogram import Router, types, F
 
 from states import BotStates, UserRegistrationStates
-from prompts import questionnaire_systemp_prompt
+from prompts import questionnaire_systemp_prompt, photo_systemp_prompt, conversation_systemp_prompt
 
 LM_API_URL = os.getenv("LM_API_URL")
 if not LM_API_URL:
@@ -25,7 +25,7 @@ router = Router()
 async def start_command(message: types.Message, state: FSMContext, db: sqlite3.Connection):
     user_id = message.from_user.id
     cursor = db.cursor()
-    cursor.execute("SELECT id FROM users WHERE id = ?", (user_id,))
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
     result = cursor.fetchone()
 
     if result:
@@ -39,8 +39,20 @@ async def start_command(message: types.Message, state: FSMContext, db: sqlite3.C
             one_time_keyboard=True
         )
         await state.set_state(BotStates.choosing_menu_state)
-        await message.answer("Привет! Я бот для оценки анкет, фотографий и ответов. Выберите, что вы хотите оценить:",
-                             reply_markup=keyboard)
+        await state.update_data({
+            "name": result[1],
+            "age": result[2],
+            "sex": result[3],
+            "zodiac": result[4],
+            "height": result[5],
+            "weight": result[6],
+            "kids": result[7],
+            "alco": result[8],
+            "tobacco": result[9]
+        })
+        await message.answer(
+            f"Привет {result[1]}! Я бот для оценки анкет, фотографий и ответов. Выберите, что вы хотите оценить:",
+            reply_markup=keyboard)
     else:
         await message.answer("Для начала просим тебя немного рассказать о себе. Напиши своё имя!")
         await state.set_state(UserRegistrationStates.name_state)
@@ -78,7 +90,6 @@ async def stop_conversation(message: types.Message, state: FSMContext):
     await message.answer("Вы вышли из режима оценки ответов.")
 
 
-# Здесь в интерактивном режиме оцениванием сообщения
 @router.message(StateFilter(BotStates.conversation_state))
 async def handle_conversation_message(message: types.Message, state: FSMContext):
     if message.text:
@@ -88,7 +99,7 @@ async def handle_conversation_message(message: types.Message, state: FSMContext)
         payload = {
             "model": "google/gemma-3-4b",
             "messages": [
-                            {"role": "system", "content": "Ты — умный помощник, отвечай на запросы на русском языке."}
+                            {"role": "system", "content": conversation_systemp_prompt}
                         ] + history,
             "temperature": 0.4
         }
@@ -110,24 +121,19 @@ async def handle_conversation_message(message: types.Message, state: FSMContext)
 
 @router.message(F.photo, StateFilter(BotStates.photo_marking_state))
 async def handle_photo(message: types.Message, state: FSMContext):
-    caption = message.caption or ""
+    caption = message.caption or "*Текст отсутствует*"
     photo = message.photo[-1]
     file = await message.bot.get_file(photo.file_id)
     bio = io.BytesIO()
     await message.bot.download_file(file.file_path, destination=bio)
     bio.seek(0)
     image_base64 = base64.b64encode(bio.read()).decode("utf-8")
-    payload = {
+    payload = payload = {
         "model": "google/gemma-3-4b",
         "messages": [
             {
                 "role": "system",
-                "content": (
-                    "Ты — умный помощник. "
-                    "Пользователь прислал фотографию, на которой изображён человек. "
-                    f"Текст под фото: «{caption}». "
-                    "Дай осмысленный ответ на русском языке."
-                )
+                "content": f"{photo_systemp_prompt}\nТекст под фото: «{caption}»"
             },
             {
                 "role": "user",
@@ -152,11 +158,37 @@ async def handle_photo(message: types.Message, state: FSMContext):
 
 @router.message(F.text, BotStates.questionnaire_marking_state)
 async def handle_text(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    if data["alco"] == -1:
+        alco = "Отрицательно"
+    elif data["alco"] == 0:
+        alco = "Нейтрально"
+    else:
+        alco = "Положительно"
+
+    if data["tobacco"] == -1:
+        tobacco = "Отрицательно"
+    elif data["tobacco"] == 0:
+        tobacco = "Нейтрально"
+    else:
+        tobacco = "Положительно"
+    additional_info = f"""
+    Вот возможная полезная информация о пользователе:
+    Имя: {data["name"]},
+    Возраст: {data["age"]},
+    Пол: {data["sex"]},
+    Знак зодиака: {data["zodiac"]},
+    Рост: {data["height"]},
+    Вес: {data["weight"]},
+    Есть ли дети: {data["kids"]},
+    Отношение к алкоголю: {alco},
+    Отношение к алкоголю: {tobacco},
+    """
     user_input = message.text
     payload = {
         "model": "google/gemma-3-4b",
         "messages": [
-            {"role": "system", "content": questionnaire_systemp_prompt},
+            {"role": "system", "content": questionnaire_systemp_prompt + "\n" + additional_info},
             {"role": "user", "content": user_input}
         ],
         "temperature": 0.4
